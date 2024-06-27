579de5e72dd325eb950f4c295af7067d58c71d30
Yiğit Bora Teke <yigitborateke@gmail.com>
2024-06-27 23:17:40 +0300

    Added blueprints for characters and character animations.

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -5,7 +5,11 @@
 
 AAuraCharacterBase::AAuraCharacterBase()
 {
- 	PrimaryActorTick.bCanEverTick = false;
+ 	PrimaryActorTick.bCanEverTick = false;	
+
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 void AAuraCharacterBase::BeginPlay()
