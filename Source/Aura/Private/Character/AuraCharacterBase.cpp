f9aea802f05bacad02ba265116108ce6ef8fdff6
Yiğit Bora Teke <yigitborateke@gmail.com>
2024-06-27 21:42:35 +0300

    Added basic character classes.

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,15 @@
+// Copyright(c) 2024 Yiğit Bora Teke. All Rights Reserved.
+
+
+#include "Character/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+ 	PrimaryActorTick.bCanEverTick = false;
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
